092ffd3bd863cc753bfd7d982c58e13be14d5c10
hopecris31 <hope.crisafi@gmail.com>
2023-03-07 02:56:09 -0500

    tokenizer

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 import torch
 import numpy as np
-from transformers import AutoTokenizer, AutoModelForCausalLM
-from datasets import load_dataset
-from transformers import DataCollatorWithPadding
-
-from transformers import TrainingArguments
-from transformers import Trainer
 import evaluate
+from datasets import load_dataset
+from sklearn.model_selection import train_test_split
+from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments, Trainer, DataCollatorWithPadding
+
 
 checkpoint = "gpt2"
 tokenizer = AutoTokenizer.from_pretrained("gpt2")
 model = AutoModelForCausalLM.from_pretrained("gpt2", pad_token_id=tokenizer.eos_token_id)
 
+# download the reddit dataset from Huggingface
 raw_datasets = load_dataset("reddit")
-raw_datasets
 
-def tokenize_function(example):
-  #NEED TO FILL THIS IN
-  return ""
+# split the datasets into a training(70%), validation(15%), and test(15%) sets.
+train_data, test_data = train_test_split(raw_datasets, test_size=0.15, random_state=42)
+train_data, val_data = train_test_split(raw_datasets, test_size=0.15, random_state=42)
+
+
+
+def tokenizer(example):
+  tokenized_input = tokenizer(example, return_tensors="pt")
+  return {"content": tokenized_input["content"][0]}
 
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
-example_inputs = ["Your name is ", "How do I ", "You drop into the world, and there lies a "]
-
-
+example_inputs = ["Your name is ", "How do I do"]
