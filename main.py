aa176cb78214d9ecf4ca964b4a83278cc7a9b41e
hopecris31 <hope.crisafi@gmail.com>
2023-03-09 16:54:08 -0500

    f

--- main.py
+++ main.py
@@ -18,20 +18,24 @@ model = AutoModelForCausalLM.from_pretrained(checkpoint)
 print("Preparing the Data")
 raw_datasets = load_dataset("reddit", split="train[:50%]")
 
+print(raw_datasets.column_names)
+
 print("Column Names: ", raw_datasets.column_names)
 
 print("Tokenizing the Data")
-
 def tokenize_function(example):
     return tokenizer(example["subreddit"], example["content"], truncation=True)
 
 tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
-
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+"""
+Dataset({
+    features: ['author', 'body', 'normalizedBody', 'subreddit', 'subreddit_id', 'id', 'content', 'summary'],
+    num_rows: 1924165
+})
+"""
 
 print("Training")
-
-
 
 def compute_metrics(eval_preds):
     metric = evaluate.load("reddit")
@@ -40,18 +44,18 @@ def compute_metrics(eval_preds):
     return metric.compute(predictions=predictions, references=labels)
 
 training_args = TrainingArguments(
-                                output_dir="./redditGPT",  # The output directory
-                                overwrite_output_dir=True,  # overwrite the content of the output directory
-                                num_train_epochs=2,
-                                per_device_train_batch_size=8,  # batch size for training
-                                per_device_eval_batch_size=16,  # batch size for evaluation
-                                eval_steps=200,  # Number of update steps between two evaluations.
-                                save_steps=400,  # after # steps model is saved
-                                warmup_steps=250,  # number of warmup steps for learning rate scheduler
-                                prediction_loss_only=True,
-                                evaluation_strategy="steps",
-                                label_names=["content", "subreddit"]
-                                )
+    output_dir="./redditGPT",  # The output directory
+    overwrite_output_dir=True,  # overwrite the content of the output directory
+    num_train_epochs=2,
+    per_device_train_batch_size=8,  # batch size for training
+    per_device_eval_batch_size=16,  # batch size for evaluation
+    eval_steps=200,  # Number of update steps between two evaluations.
+    save_steps=400,  # after # steps model is saved
+    warmup_steps=250,  # number of warmup steps for learning rate scheduler
+    prediction_loss_only=True,
+    evaluation_strategy="steps",
+    #label_names=["logits", "past_key_values"]
+    )
 
 trainer = Trainer(
     model=model,
@@ -59,10 +63,10 @@ trainer = Trainer(
     train_dataset=tokenized_datasets,
     data_collator=data_collator,
     tokenizer=tokenizer,
-    compute_metrics=compute_metrics)
+    compute_metrics=compute_metrics
+    )
 
 trainer.train()
-path = "C:/Users/hopec/Desktop/NLP_Transformer"
-trainer.save_model(path + "/ChatGRT")
+path = "C:/Users/hopec/Desktop/NLP_Transformer/ChatGRT"
+trainer.save_model(path)
 
-trainer.evaluate(tokenized_datasets)
